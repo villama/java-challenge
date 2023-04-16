cd971e3abaf5f3e328d9297767f1e4098bcd72bf
Julius Villamayor <julius.villamayor@roms.inc>
2023-04-16 13:29:29 +0900

    Add javadoc comments

--- src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
+++ src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
@@ -8,34 +8,69 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * EmployeeServiceImpl is a service class that provides
+ * operations related to Employee management.
+ */
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    /**
+     * Sets the employee repository to be used by this service.
+     *
+     * @param employeeRepository the employee repository
+     */
     public void setEmployeeRepository(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
 
+    /**
+     * Retrieves a list of all employees.
+     *
+     * @return a list of employees
+     */
     public List<Employee> retrieveEmployees() {
         List<Employee> employees = employeeRepository.findAll();
         return employees;
     }
 
+    /**
+     * Retrieves an employee by ID.
+     *
+     * @param employeeId the employee ID
+     * @return the employee with the specified ID, or null if not found
+     */
     public Employee getEmployee(Long employeeId) {
         Optional<Employee> optEmp = employeeRepository.findById(employeeId);
         return optEmp.get();
     }
 
+    /**
+     * Saves a new employee.
+     *
+     * @param employee the employee to save
+     */
     public void saveEmployee(Employee employee){
         employeeRepository.save(employee);
     }
 
+    /**
+     * Deletes an employee by ID.
+     *
+     * @param employeeId the employee ID to delete
+     */
     public void deleteEmployee(Long employeeId){
         employeeRepository.deleteById(employeeId);
     }
 
+    /**
+     * Updates an existing employee with new information.
+     *
+     * @param employee the employee with updated information
+     */
     public void updateEmployee(Employee employee) {
         employeeRepository.save(employee);
     }
